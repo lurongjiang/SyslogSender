dec456decf56818e28d47408795546178c05c515
GitHub Action <action@github.com>
2022-06-20 06:32:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/lurongjiang/syslogsender/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lurongjiang/syslogsender/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lurongjiang.syslogsender.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lurongjiang.syslogsender.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lurongjiang/syslogsender/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lurongjiang/syslogsender/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lurongjiang.syslogsender.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lurongjiang.syslogsender.MyBundle
 
 class MyProjectService(project: Project) {
 
